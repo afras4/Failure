55a7b8d11dfb0faccd7d40792ddfef8914f39599
Andres <afrias4b@hotmail.com>
2021-02-26 15:32:45 -0500

    scripts

---
+++ Assets/Scripts/MovimientoP1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoP1 : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        Vector3 movimiento= new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position += movimiento*speed*Time.deltaTime;
+    }
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(0f,5f), ForceMode2D.Impulse);
+        }
+    }
+    
+}
