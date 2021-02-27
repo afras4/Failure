1b3fa4faac816bc946095f588ebd7cafee5adb82
Cadeteete <50933516+Cadeteete@users.noreply.github.com>
2021-02-27 17:13:19 -0500

    Cambio de Nombre y Menú

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject main;
+    void Start()
+    {
+        panel.SetActive(false);
+        main.SetActive(true);
+    }
+    public void Credits(){
+        panel.SetActive(true);
+        main.SetActive(false);
+    }
+    public void Back(){
+        panel.SetActive(false);
+        main.SetActive(true);
+    }
+    public void LoadScene(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
