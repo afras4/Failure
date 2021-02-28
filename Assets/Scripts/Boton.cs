2dc9987ae2bdee0f10444ad44f98c5ee98ee3b6a
Andres <afrias4b@hotmail.com>
2021-02-28 03:46:05 -0500

    scripts botones

---
+++ Assets/Scripts/Boton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{
+    public GameObject [] posicionesrojo;
+    public GameObject [] posicionesazul;
+
+    void Start()
+    {
+        spawn();
+    }
+    void Update()
+    {
+    }
+    public void spawn()
+    {
+        int ran=Random.Range(1, 3);
+        int ran2=Random.Range(1,3);
+        Debug.Log("  " + ran);
+        Debug.Log("  " + ran2);
+        switch(ran)
+        {
+            case 1:
+             posicionesrojo[0].SetActive(true);
+            break;
+            
+            case 2:
+            posicionesrojo[1].SetActive(true);
+            break;
+            /*
+            case 3:
+            gameObject.transform.position= posiciones[2].transform.position;
+            break;
+
+            case 4:
+            gameObject.transform.position= posiciones[3].transform.position;
+            break;
+            */
+        }
+        switch(ran2)
+        {
+            case 1:
+             posicionesazul[0].SetActive(true);
+            break;
+            
+            case 2:
+             posicionesazul[1].SetActive(true);
+            break;
+        }
+    }
+}
--- Assets/Scripts/Botones.cs
+++ Assets/Scripts/Botones.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class Botones : MonoBehaviour
 {
     public DeathObject deathObject;
-    public GameObject [] botones;
-    public GameObject[] activar;
     public int colorButton;
+    public GameObject funcion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     private void OnCollisionEnter2D(Collision2D other) {
         if(colorButton == 1){
         if(other.gameObject.tag == "Player1"){
             deathObject.color = 1;
-            activar[1].SetActive(true);
-            botones[0].SetActive(false);
-            botones[1].SetActive(false);
-            botones[2].SetActive(false);
+            funcion.GetComponent<Boton> ().spawn();
             this.gameObject.SetActive(false);
             
         }    
@@ -24,10 +30,7 @@ public class Button : MonoBehaviour
         if(colorButton == 2){
         if(other.gameObject.tag == "Player2"){
             deathObject.color = 2;
-            activar[0].SetActive(true);  
-            botones[0].SetActive(false);
-            botones[1].SetActive(false);
-            botones[2].SetActive(false);
+            funcion.GetComponent<Boton> ().spawn();
             this.gameObject.SetActive(false);
               
         }
